2eb8a6543fe2c127065adecc646be1870a25cc76
Vietnguyencong <vietnguyencong99@gmail.com>
2021-01-29 22:00:58 -0600

    decoratin the app and add env variable

--- index.js
+++ index.js
@@ -3,35 +3,24 @@ const webPush = require('web-push');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express()
-app.use(bodyParser.json())
-const public_key = 'BMnatDbIVtcS42bMxhpWgQjRJlyQuMxGIx4dU6J9neItwSnJ71ZPq79tvuv90Oqfvr-6pjxeuYNWs84VjWX7Ge8'
-const private_key = 'ju6iAB70um95vZmojT1gUr4T_ortSZLk75aBA70-vzc'
-const axios = require('axios')
-
 const fetch = require('node-fetch');
+require('dotenv').config()
 
+app.use(bodyParser.json())
 
 // set Static path 
 app.use(express.static(path.join(__dirname, "client")))
 
 webPush.setVapidDetails(
     'mailto:test@test.com',
-    public_key,
-    private_key, 
+    process.env.public_key,
+    process.env.private_key, 
 )
 
-const get_image = axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 'Client-ID H4omZ7vkNKxVOly_B6COajPhUtDpWHNPa_z475pEC54'
-    }
-})
 
 app.get("/getimage", async (req,res)=>{
 
-    // const viet = await get_image.get('/search/photos', {
-    //     params: {query: "vietnam"},
-    // })
+
     const url = 'https://api.unsplash.com/search/photos?'
     const query = "vietnam"
     const viet = await fetch(url + `query=${query}`,{
@@ -44,7 +33,7 @@ app.get("/getimage", async (req,res)=>{
     res.json(json)    
 })
 
-// send teh subcribe route 
+// send the subcribe route 
 app.post('/subscribe',  (req,res)=>{
     // get pushSubscription object
     const subscription = req.body
